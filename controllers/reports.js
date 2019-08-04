8f4a40fabd51ca1e9c3d6129849b0864fd8d733b
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-08-04 16:06:11 +0200

    Just linter!

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,56 +1,62 @@
-const processFile = require("../lib/batimagen"),
-uuidv4 = require('uuid/v4'),
-{fileValidation} = require("../lib/digester"),
-{reports} = require('../lib/database');
+const processFile = require('../lib/batimagen')
+const uuidv4 = require('uuid/v4')
+const { fileValidation } = require('../lib/digester')
+const { reports } = require('../lib/database')
 
 function home (req, res) {
-    res.render('index');
+  res.render('index')
 }
 
 function upload (req, res) {
-  if(!fileValidation(req)) {
-    return res.status(400).render('error', { error: 'No files were uploaded.'});
+  if (!fileValidation(req)) {
+    return res.status(400).render('error', { error: 'No files were uploaded.' })
   }
   const uuid = uuidv4()
-  processFile(req, {uuid})
+  processFile(req, { uuid })
   res.redirect(`/reports/${uuid}`)
 }
 
 function getList (req, res) {
-    res.render('reports')
+  res.render('reports')
 }
 
 function remove (req, res) {
-  const uuid = req.body.uuid;
-  if(!uuid) {
-    return res.status(400).render('error', { error: "Bas request! Missing UUID!"})
+  const uuid = req.body.uuid
+  if (!uuid) {
+    return res
+      .status(400)
+      .render('error', { error: 'Bas request! Missing UUID!' })
   }
 
-  const report = reports.getOne(uuid);
-  if(!report) {
-    return res.status(404).render('error', { error: "This report does not exist!"})
-  } 
-  
-  if(!report.isReady){
-    return res.status(400).render('error', { error: "This report wasn't generated yet!"})
-  } 
-  
+  const report = reports.getOne(uuid)
+  if (!report) {
+    return res
+      .status(404)
+      .render('error', { error: 'This report does not exist!' })
+  }
+
+  if (!report.isReady) {
+    return res
+      .status(400)
+      .render('error', { error: "This report wasn't generated yet!" })
+  }
+
   removeRelatedFiles(uuid)
     .then(() => {
       reports.remove(uuid)
       res.redirect('/reports')
     })
-    .catch(err => res.status(500).render('error', { error: err}))
+    .catch(err => res.status(500).render('error', { error: err }))
 }
 
 function getDetail (req, res) {
-    const uuid = req.params.uuid;
-    const report = reports.getOne(uuid)
-    if(report && report.isReady){
-      res.render('results', {data: report.data})
-    } else {
-      res.redirect('/reports')
-    }
+  const uuid = req.params.uuid
+  const report = reports.getOne(uuid)
+  if (report && report.isReady) {
+    res.render('results', { data: report.data })
+  } else {
+    res.redirect('/reports')
   }
+}
 
-module.exports = {home, upload, remove, getList, getDetail}
+module.exports = { home, upload, remove, getList, getDetail }
